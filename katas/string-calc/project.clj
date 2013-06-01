b255cdbc095d1c217142176bf3909f39bc46c91c
Jerry Mindek <jerry.mindek@gmail.com>
2013-05-31 23:02:47 -0400

    Removed midje from project since I am sticking with clojure.testing for now. Steps 1 through 3 done. I skipped four since my heuristic does not care about delims. I am in-progress on step 5.

--- katas/string-calc/project.clj
+++ katas/string-calc/project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-alpha "0.0.9-SNAPSHOT"
   :description "String calculator kata"
   :dependencies [[org.clojure/clojure "1.5.0"]
-  				 [midje "1.5.1"]]
+  				 ]
 	:main string-calc.core)
--- katas/string-calc/test/string_calc/core_test.clj
+++ katas/string-calc/test/string_calc/core_test.clj
@@ -2,22 +2,37 @@
   (:use clojure.test
         string-calc.core))
 
-'(deftest a-test
-'  (testing "FIXME, I fail."
-'    (is (= 0 1))))
-
-(deftest add-return-0-for-empty-str-test
-	(testing "#add function does not return 0 for empty string.")
+(deftest empty-str-test
+	(testing "#return 0 for empty string.")
 	(is (= 0 (add ""))))
 
-(deftest add-return-num-for-str-with-1-num-test
-	(testing "add function returns function argument when single argument received")
+(deftest str-with-1-num-test
+	(testing "return function argument when single argument received")
 	(is (= 1 (add "1"))))
 
-(deftest add-return-num-for-str-with-2-num-test
-	(testing "add function returns sum of individual nums in string")
-	(is (= 2 (add "11"))))
+(deftest str-with-2-num-test
+	(testing "returns sum of individual nums in string")
+	(is (= 2 (add "1,1"))))
 
-(deftest add-return-num-for-str-with-6-num-test
-	(testing "add function returns sum of individual nums in string")
-	(is (= 6 (add "111111"))))
+(deftest str-with-6-num-test
+	(testing "returns sum of individual nums in string")
+	(is (= 6 (add "1,1,1,1,1,1"))))
+
+(deftest str-with-newline-delim-num-test
+	(testing "returns sum of individual nums in string delimited by newline")
+	(is (= 3 (add "1\n1\n1"))))
+
+'(deftest add-set-delim-test
+'	(testing "determine delimeter based on string then add")
+'	(is (= 3 (add "//;\n1;1;1"))))
+
+(deftest neg-ex-test
+	(testing "should return excpetion message for negitive number")
+	(is (= "Negitives not allowed: -1" (neg-ex -1))))
+
+(comment
+(deftest str-with-negitive-num-test
+	(testing "returns exception Negitives not allowed and displays the negitive numbers")
+	(is (= "Negitives not allowed: -1" (add "1\n-1\n1"))))
+)
+
