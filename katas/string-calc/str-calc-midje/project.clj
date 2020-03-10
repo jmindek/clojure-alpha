70ce382a18910eabdb3e81c255e0614be648f25e
Jerry Mindek <jerry.mindek@averinformatics.com>
2020-03-09 21:48:10 -0400

    Remove path that had starter app code only

--- katas/string-calc/str-calc-midje/project.clj
+++
@@ -1,5 +0,0 @@
-(defproject str-calc-midje "0.1.0-SNAPSHOT"
-  :description "str-calc using midje testing framework"
-  :url "https://github.com/jmindek/clojure-alpha/tree/master/katas/string-calc/str-calc-midje"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
-  :main str-calc-midje.core)
